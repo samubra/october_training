c89a4a287dc2d026aac2ba02d11d84bae2eb0ed1
samubra <samubra@live.cn>
2019-03-29 07:39:19 +0000

    完善证书页面

--- Plugin.php
+++ Plugin.php
@@ -8,20 +8,34 @@ use Illuminate\Foundation\AliasLoader;
 use RainLab\User\Models\User as UserModel;
 use RainLab\User\Controllers\Users as UsersController;
 
+
 class Plugin extends PluginBase
 {
     public $require = ['RainLab.User'];
 
     public function boot()
     {
+        \App::register(\Propaganistas\LaravelPhone\PhoneServiceProvider::class);
+        \App::register('\Maatwebsite\Excel\ExcelServiceProvider');
+
+        // Register aliases
+        $alias = AliasLoader::getInstance();
+        $alias->alias('Excel', 'Maatwebsite\Excel\Facades\Excel');
+
         Validator::extend('identity', function($attribute, $value, $parameters, $validator) {
-            return preg_match('/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}$)/', $value);
+            //return preg_match('/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}$)/', $value);
+           $idValidator = new \Jxlwqq\IdValidator\IdValidator();
+           return $idValidator->isValid($value);
         });
+        /**
         Validator::extend('phone', function($attribute, $value, $parameters, $validator) {
             //return preg_match('/^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$/',$value);
             return preg_match('/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$/',$value);
         });
+         * /*/
     $this->extendUser();
+
+
 
     }
 
@@ -70,7 +84,7 @@ class Plugin extends PluginBase
                 'identity'
             ]);
 
-            $model->rules['identity'] = 'identity|unique:users';
+            $model->rules['identity'] = ['identity','unique:users'];
 
             $model->attributeNames['identity'] = '身份证号码';
         });

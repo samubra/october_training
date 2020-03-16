00423de79e00de27bd7456c2ad0cfb1d3f3f2e9a
samubra <samubra@live.cn>
2020-03-16 10:50:53 +0800

    后台列表复制和禁启用

--- controllers/Plans.php
+++ controllers/Plans.php
@@ -1,6 +1,8 @@
 <?php namespace Samubra\Training\Controllers;
 
 use BackendMenu;
+use Samubra\Training\Models\Plan;
+use Flash;
 
 class Plans extends TrainingController
 {
@@ -12,7 +14,7 @@ class Plans extends TrainingController
     public $controllerName = 'plans';
     public $controllerTitle = '培训方案';
     public $requiredPermissions = ['samubra.training.access_plan'];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $relationConfig = 'config_relation.yaml';
@@ -22,5 +24,33 @@ class Plans extends TrainingController
     {
         parent::__construct();
         BackendMenu::setContext('Samubra.Training', 'plans', 'plans');
+    }
+
+    public function onCopy()
+    {
+        if(($checked = post('checked')) && is_array($checked))
+        {
+            $count = 0;
+            foreach ($checked as $planId){
+                $count++;
+               $oldPlan = Plan::with('courses')->find($planId);
+               $newPlan = $oldPlan->replicate();
+               $newPlan->title = $oldPlan->title . ' 副本'.$count;
+               $newPlan->push();
+               //$newPlan->save();
+               if($oldPlan->courses->count()){
+                   foreach ($oldPlan->courses as $course){
+                       $newPlan->courses()->attach($course);
+                   }
+               }
+
+                unset($oldPlan);
+                unset($newPlan);
+            }
+            Flash::success('已经为所选择的'.$count.'个培训方案创建了副本！');
+        }else{
+            Flash::error('请选择需要复制的培训方案！');
+        }
+        return $this->listRefresh();
     }
 }
--- controllers/Projects.php
+++ controllers/Projects.php
@@ -1,6 +1,9 @@
 <?php namespace Samubra\Training\Controllers;
 
 use BackendMenu;
+use Samubra\Training\Models\Project;
+use Flash;
+
 
 class Projects extends TrainingController
 {
@@ -13,7 +16,7 @@ class Projects extends TrainingController
     public $controllerName = 'projects';
     public $controllerTitle = '培训项目';
     public $requiredPermissions = ['samubra.training.access_project'];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $relationConfig = 'config_relation.yaml';
@@ -25,5 +28,64 @@ class Projects extends TrainingController
         parent::__construct();
         $this->addCss('/plugins/samubra/training/assets/backend.css');
         BackendMenu::setContext('Samubra.Training', 'projects', 'projects');
+    }
+
+
+    public function onCopy()
+    {
+        if(($checked = post('checked')) && is_array($checked))
+        {
+            $count = 0;
+            foreach ($checked as $projectId){
+                $count++;
+                $oldProject = project::with('courses')->find($projectId);
+                $newProject = $oldProject->replicate();
+                $newProject->title = $oldProject->title . ' 副本'.$count;
+                $newProject->slug = $oldProject->slug . 'copy'.$count.'rand'.rand(1,100);
+                $newProject->push();
+                //$newProject->save();
+                if($oldProject->courses->count()){
+                    foreach ($oldProject->courses as $course){
+                        $newCourse = $course->replicate();
+                        $newCourse->push();
+                        $newCourse->project()->associate($newProject);
+                        $newCourse->save();
+                    }
+                }
+                unset($oldProject);
+                unset($newProject);
+            }
+            Flash::success('已经为所选择的'.$count.'个培训项目创建了副本！');
+        }else{
+            Flash::error('请选择需要创建副本的培训项目！');
+        }
+        return $this->listRefresh();
+    }
+
+    public function onActive()
+    {
+        if (
+            ($projectIds = post('checked')) &&
+            is_array($projectIds)
+        ) {
+            Project::whereIn('id',$projectIds)->update(['active'=>true]);
+            Flash::success('所选择的培训项目已设置为启用');
+        }else{
+            Flash::error('请选择需要启用的培训项目进行操作！');
+        }
+        return $this->listRefresh();
+    }
+    public function onNotActive()
+    {
+        if (
+            ($projectIds = post('checked')) &&
+            is_array($projectIds)
+        ) {
+            Project::whereIn('id',$projectIds)->update(['active'=>false]);
+            Flash::success('所选择的培训项目已设置为禁用');
+        }else{
+            Flash::error('请选择需要禁用的培训项目进行操作！');
+        }
+        return $this->listRefresh();
     }
 }
